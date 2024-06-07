322ab4301ec558108bc168d50e8a42b290ffa9bd
tpring <pororo1@naver.com>
2024-06-07 12:32:51 +0900

    2024.06.07 fe/update/#59 신자영

--- src/components/MyPage/Profile.jsx
+++ src/components/MyPage/Profile.jsx
@@ -3,43 +3,42 @@ import ProfilePhoto from './ProfilePhoto';
 import Navigation from './Navigation';
 import { useProfile } from '../../contexts/ProfileContext';
 
-const Profile = () => {
-  const { profile } = useProfile();
-  
+const Profile = ({ userId }) => {
+    const { profile } = useProfile();
 
+    if (!profile) {
+        return <div>로딩 중...</div>;
+    }
 
-  // console.log("Profile in Profile component:", profile);
+    const postCount = profile.posts ? profile.posts.length : 0;
+    const followerCount = profile.followers ? profile.followers.length : 0;
+    const followingCount = profile.following ? profile.following.length : 0;
 
-  if (!profile) {
-    return <div>로딩 중...</div>;
-  }
-
-  const postCount = profile.posts ? profile.posts.length : 0;
-  const followerCount = profile.followers ? profile.followers.length : 0;
-  const followingCount = profile.following ? profile.following.length : 0;
-
-  return (
-    <ProfileContainer>
-      <NavigationAndPhotoContainer>
-        <ProfilePhoto src={profile.photo || 'default_image_url'} />
-        <Navigation postCount={postCount} followerCount={followerCount} followingCount={followingCount} />
-      </NavigationAndPhotoContainer>
-    </ProfileContainer>
-  );
+    return (
+        <ProfileContainer>
+            <NavigationAndPhotoContainer>
+                <ProfilePhoto src={profile.photo || 'default_image_url'} />
+                <Navigation
+                    userInfo={userId}
+                    initialPostCount={postCount}
+                    initialFollowerCount={followerCount}
+                    initialFollowingCount={followingCount}
+                />
+            </NavigationAndPhotoContainer>
+        </ProfileContainer>
+    );
 };
 
 export default Profile;
 
-
-// 스타일링된 컨테이너 생성
 const ProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const NavigationAndPhotoContainer = styled.div`
-  display: flex; 
-  align-items: center;
-  justify-content: center;
-  margin-right: 20%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20%;
 `;
