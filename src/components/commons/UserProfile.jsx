1f5c0489b5f4eb91991807121a13a41acc7fd8d5
Minkon Kim <alsrhs98@gmail.com>
2024-06-07 14:41:44 +0900

    0607 자잘자잘 수정

--- src/components/commons/UserProfile.jsx
+++ src/components/commons/UserProfile.jsx
@@ -1,70 +1,78 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import supabase from '../../api/supabase.client';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import supabase from "../../api/supabase.client";
 
 const UserProfile = ({ userId }) => {
-    const navigate = useNavigate();
-    const [userInfo, setUserInfo] = useState([]);
-    useEffect(() => {
-        // Supabase에서 유저 정보 가져오기
-        const fetchUserInfo = async () => {
-            const { data, error } = await supabase.from('userInfo').select('*').eq('id', userId).single(); // 단일 유저 정보만 필요!!!
-            if (error) {
-            } else {
-                setUserInfo(data);
-            }
-        };
-        fetchUserInfo();
-    }, [userId]);
-
-    // console.log("유저프로필", userId);
-    const handleClick = () => {
-        if (userInfo) {
-            navigate(`/mypage/${userInfo.id}`);
-        }
+  const navigate = useNavigate();
+  const [userInfo, setUserInfo] = useState([]);
+  useEffect(() => {
+    // Supabase에서 유저 정보 가져오기
+    const fetchUserInfo = async () => {
+      const { data, error } = await supabase
+        .from("userInfo")
+        .select("*")
+        .eq("id", userId)
+        .single(); // 단일 유저 정보만 필요!!!
+      if (error) {
+        console.error(error);
+      } else {
+        setUserInfo(data);
+      }
     };
+    fetchUserInfo();
+  }, [userId]);
 
-    return (
-        <>
-            {userInfo && (
-                <UserProfileContainer onClick={handleClick}>
-                    <UserAvatar src={userInfo.avatar_url} alt={`${userInfo.nickname}'s avatar`} />
-                    <UserInfo>
-                        <UserNickname>@{userInfo.nickname}</UserNickname>
-                    </UserInfo>
-                </UserProfileContainer>
-            )}
-        </>
-    );
+  // console.log("유저프로필", userId);
+  const handleClick = () => {
+    if (userInfo) {
+      navigate(`/mypage/${userInfo.id}`);
+    }
+  };
+
+  return (
+    <>
+      {userInfo && (
+        <UserProfileContainer onClick={handleClick}>
+          <UserAvatar
+            src={userInfo.avatar_url}
+            alt={`${userInfo.nickname}'s avatar`}
+          />
+          <UserInfo>
+            <UserNickname>{userInfo.nickname}</UserNickname>
+          </UserInfo>
+        </UserProfileContainer>
+      )}
+    </>
+  );
 };
 
 const UserProfileContainer = styled.div`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    padding: 10px;
-    transition: background-color 0.3s;
-    &:hover {
-        background-color: #f0f0f0;
-    }
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 10px;
+  transition: background-color 0.3s;
+  &:hover {
+    background-color: #f0f0f0;
+  }
 `;
 
 const UserAvatar = styled.img`
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-right: 15px;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 15px;
 `;
 
 const UserInfo = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const UserNickname = styled.div`
-    font-size: 14px;
-    color: #6c757d;
+  font-size: 14px;
+  color: #6c757d;
 `;
 
 export default UserProfile;
